7e3761ad9f888b3301e90f2468d4f68f67a04f15
sahilKharwade <skharwade.smk@gmail.com>
2020-12-31 13:03:26 +0530

    First api called for list of countries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-
-import React from "react";
+import React ,{useState,useEffect} from "react";
+import "./App.css";
 import {MenuItem,FormControl,Select } from '@material-ui/core';
 // import{MenuItem,FormControl,Select} from "@material_ui/core"
 
 function App() {
+
+
+    const [countries, setCountries]= 
+    useState([]);
+    // State = how to write a variable in react
+
+
+    // api used from disease.sh
+
+
+    // USEEFFECT = runs piece of code base on given condition
+    // at end there is square bracket in which we use variable 
+    // when that variable is change useeffect run again with
+    useEffect(() =>{
+        // async -> send a request , wait for it , do something with it 
+        const getCountiresData = async () =>{
+            await fetch("https://disease.sh/v3/covid-19/countries")
+            .then((response) => response.json())
+            .then((data) =>{
+                const countries = data.map((country) =>({
+                    name: country.country,
+                    value: country.countryInfo.iso2
+                }));
+                setCountries(countries);
+            });
+
+
+        };
+        getCountiresData();
+
+    },[]);
+
+
+
+
   return (
     <div className="app">
-     <h1>COVID-19 TRACKER</h1>
-     <FormControl className="app__dropdown">
-     {/* // BEM naming convention in which first is component and second is element */}
-      <Select
-        varient="outlined"
-        value="abc"
-      >
-      <MenuItem value="worldwide"> Worldwide</MenuItem>
-      <MenuItem value="worldwide"> wide</MenuItem>
-      <MenuItem value="worldwide"> World</MenuItem>  
+     <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+        {/* // BEM naming convention in which first is component and second is element */}
+            <Select
+                varient="outlined"
+                value="abc">
+            
+                {/* {we loop though country and show here} */}
 
 
-      </Select>
-     </FormControl>
+
+                {countries.map(country=> (
+                        <MenuItem value={country.value}>{country.name}</MenuItem>
+                ))}
+
+                {/* <MenuItem value="worldwide"> Worldwide</MenuItem>
+                <MenuItem value="worldwide"> wide</MenuItem>
+                <MenuItem value="worldwide"> World</MenuItem>   */}
+            </Select>
+        </FormControl>
+
+
+
+     </div>
+   
+     
      {/* {Header} */}
      {/* {Title + Select input dropdown field} */}
 
