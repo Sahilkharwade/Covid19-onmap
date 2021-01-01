036a9aa27b8b5bbd10a9405a29fdcd4b1ee6cd04
sahilKharwade <skharwade.smk@gmail.com>
2021-01-01 14:07:48 +0530

    InfoBox with live data done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React ,{useState,useEffect} from "react";
 import "./App.css";
-import {MenuItem,FormControl,Select } from '@material-ui/core';
-// import{MenuItem,FormControl,Select} from "@material_ui/core"
+import {MenuItem,FormControl,Select ,Card ,CardContent} from '@material-ui/core';
+//import{MenuItem,FormControl,Select} from "@material_ui/core"
+import InfoBox from './InfoBox'; 
+import Map from './Map';
 
 function App() {
-
-
+     
+                    
     const [countries, setCountries]= 
     useState([]);
     const [country, setCountry]=useState("worldwide");
-    // State = how to write a variable in react
+    const [countryInfo, setCountryInfo]=useState([]);
+    //State = how to write a variable in react
 
+
+    // for worldwide data just after loading 
+
+    useEffect( () => {
+        fetch("https://disease.sh/v3/covid-19/all").then( (response) => response.json())
+        .then((data) => {
+            setCountryInfo(data);
+        });
+
+
+
+    },[]);
 
     // api used from disease.sh
 
-
+    
     // USEEFFECT = runs piece of code base on given condition
     // at end there is square bracket in which we use variable 
     // when that variable is change useeffect run again with
@@ -41,12 +56,29 @@ function App() {
     const onCountryChange = async(event)=>{
         const countryCode=event.target.value;
         // console.log(countryCode);
-        setCountry(countryCode);
+        // setCountry(countryCode);
+
+        const url = countryCode ==='worldwide' ? "https://disease.sh/v3/covid-19/all" :
+        `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+        // {/* for world wide date the is diffrent api call so for that we use if else */}
+        // https://disease.sh/v3/covid-19/all  that gives worldwide data 
+        // https://disease.sh/v3/covid-19/countries/[COUNTRY_CODE] that gives all info about country
+
+        await fetch(url).then(response => response.json()).then(data=>{
+            setCountry(countryCode);
+
+            setCountryInfo(data);
+
+
+        });
+
     };
+    console.log("country info >>>>>",countryInfo);
 
 
   return (
     <div className="app">
+    <div className="app_left">
      <div className="app__header">
         <h1>COVID-19 TRACKER</h1>
         <FormControl className="app__dropdown">
@@ -77,17 +109,43 @@ function App() {
      {/* {Title + Select input dropdown field} */}
 
 
+    <div className="app__stats">
+
+                <InfoBox title="Coronavirus Cases" cases={countryInfo.todayCases} total={countryInfo.cases}/>
+                <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered}/> />
+                <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
+                
+    
+                {/* {Info Boxs} */}
+                {/* {Info Boxs} */}
+                {/* {Info Boxs} */}
+
+
+    </div>
       
-     {/* {Info Boxs} */}
-     {/* {Info Boxs} */}
-     {/* {Info Boxs} */}
 
 
-
-     {/* {Table} */}
-     {/* {Graph} */}
 
      {/* {Map} */}
+     <Map />
+
+
+    </div>
+    <Card className="app__right">
+                <CardContent>
+                    {/* {Table} */}
+                    <h3> Live cases by Country</h3>
+                    {/* {Graph} */}
+                    <h3>Worldwide new cases</h3>
+                </CardContent>
+                
+        
+     
+     
+
+    </Card>
+
+
     </div>
   );
 }
---
+++ src/InfoBox.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Card,CardContent,Typography} from "@material-ui/core"
+function InfoBox({title,cases,total}) {
+    return (
+      <Card  className="infoBox">
+          <CardContent>
+            {/* {Title} */}
+            <Typography className="infoBox__title" color="textSecondary">
+                {title}
+            </Typography>
+
+            {/* NO of cases */}
+            <h2 className="infoBox__cases">{cases}</h2>
+
+            {/* Total cases */}
+            <Typography  className="infoBox__total"color="textSecondary">>
+                {total} Total
+            </Typography>
+
+          </CardContent>
+      </Card>
+    )
+}
+
+export default InfoBox
